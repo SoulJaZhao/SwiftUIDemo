9c679b07d3392e8380d457f2e10e9c0f6dcb34b4
zhaolong <superzhaolong@126.com>
2025-10-13 20:36:23 +0800

    Guess the flag

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by 赵龙 on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
