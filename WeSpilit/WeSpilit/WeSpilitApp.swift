ea665a15def2da920b9c68c81ee803534d9b344c
zhaolong <superzhaolong@126.com>
2025-10-13 19:40:07 +0800

    WeSplit App

---
+++ WeSpilit/WeSpilit/WeSpilitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSpilitApp.swift
+//  WeSpilit
+//
+//  Created by Long Zhao on 2025/10/13.
+//
+
+import SwiftUI
+
+@main
+struct WeSpilitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
