fbc2ec44826e3243776e1f53e54e33b670b3c926
zhaolong <superzhaolong@126.com>
2025-12-09 14:52:03 +0800

    Flashzilla

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by 赵龙 on 2025/12/9.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
